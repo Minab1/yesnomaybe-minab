dd57cd1110120cdc19c9f4851dbb82fea67287fd
Minab1 <92942758+Minab1@users.noreply.github.com>
2021-11-08 17:43:28 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("yess!")
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showString("maybeee!")
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("nooo!")
+})
